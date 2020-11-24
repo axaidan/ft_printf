ceeef5cf04e58d1abb38f9e0ad6ffbd58ebbb753
axaidan <axaidan@hotmail.com>
2020-11-24 14:11:35 +0100

    fixing '%' try2

--- print_char.c
+++ print_char.c
@@ -6,7 +6,7 @@
 /*   By: axaidan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 16:44:47 by axaidan           #+#    #+#             */
-/*   Updated: 2020/11/24 14:04:19 by axaidan          ###   ########.fr       */
+/*   Updated: 2020/11/24 14:11:07 by axaidan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int		print_char(t_substr conv, va_list args, char c)
 	int	i;
 
 	// SHOULD BE UNSIGNED CHAR, putchar_ret() TAKES CHARS FOR NOW
-//	if (c == '%')
-//		return (putchar_ret('%'));
 	conv.c = (c == '%') ? '%' : (unsigned char)va_arg(args, int);
 	i = 0;
 	if (!(conv.f_minus))
 		while (i < conv.width - 1)
-			i += putchar_ret(' ');
+			i += (conv.f_zero) ? putchar_ret('0') : putchar_ret(' ');
 	i += putchar_ret(conv.c);
 	if (conv.f_minus)
 		while (i < conv.width)
